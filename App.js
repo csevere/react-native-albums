dd2b5ffa44583bbfe3a213914fdcb586cf358be4
csevere <carla.severe@gmail.com>
2017-10-03 03:40:38 -0400

    created the header for music app

--- App.js
+++ App.js
@@ -1,21 +1,33 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { AppRegistry, StyleSheet, Text, View } from 'react-native';
+import Header from './src/components/Header'; 
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Hello World!</Text>
-      </View>
-    );
-  }
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#D3D3D3',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const App = () =>(
+    <Header headerText = {'Albums'}/> 
+);
+
+
+//register at least one component 
+AppRegistry.registerComponent('react-native-albums', () => App);
+
+// export default class App extends React.Component {
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <Text>Hello World!</Text>
+//       </View>
+//     );
+//   }
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#D3D3D3',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+
+export default App; 
