32d276677fc349c300e0adaa46fc5320c9bc1e38
csevere <carla.severe@gmail.com>
2017-10-05 15:03:02 -0400

    added button and styles

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Text, View, Image } from 'react-native'; 
 import Card from './Card';
 import CardSection from './CardSection';
+import Buttons from './Buttons';
 
 const AlbumDetail = ({album}) =>{
     const {title, artist, thumbnail_image, image} = album;
@@ -23,16 +24,23 @@ const AlbumDetail = ({album}) =>{
                         source = {{uri:thumbnail_image }} 
                     />
                 </View>
+                
                 <View style = {headerContentStyle}>
                     <Text style = {headerTextStyle}>{title}</Text>
                     <Text>{artist}</Text> 
                 </View>
             </CardSection> 
+
             <CardSection>
                 <Image 
                     style = {imageStyle} 
                     source = {{uri:image}} 
                 />
+            </CardSection>
+
+            <CardSection>
+                <Buttons/>
+        
             </CardSection>
         </Card>
     )
---
+++ src/components/Buttons.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import {Text, TouchableOpacity} from 'react-native';
+
+
+const Buttons = () => {
+    const {buttonStyle, textStyle} = styles; 
+    return(
+        <TouchableOpacity style = {buttonStyle}>
+            <Text style = {textStyle}>
+                Buy Album
+            </Text>
+        </TouchableOpacity>
+    );
+};
+
+const styles = {
+    textStyle:{
+        alignSelf: 'center',
+        color: '#007aff',
+        fontSize: 16,
+        fontWeight: '600',
+        paddingTop: 10,
+        paddingBottom: 10
+
+    },
+
+    buttonStyle: {
+        flex:1,
+        alignSelf: 'stretch',
+        backgroundColor: '#fff',
+        borderWidth: 1,
+        borderRadius: 5, 
+        borderColor: '#007aff',
+        marginLeft: 5,
+        marginRight: 5
+    }
+};
+
+export default Buttons 
